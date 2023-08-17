35af86e66466393e8d6fbd87583600febb0bacfe
jaime <garciadiazjaime@gmail.com>
2023-08-16 23:28:49 -0500

    feat: update links

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,40 +1,55 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
-<style>
-	main, footer {
-		position: relative;
-		margin: 0 auto;
-		box-sizing: border-box;
-		height: auto;
-		max-width: 960px;
-		margin: 0 auto;
-	}
-</style>
-
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
 
 <footer>
-	<div>
-		Síguenos en:
-		<ul>
-			<li><a href="https://www.instagram.com/playamitijuana/" title="Restaurantes en Playas de Tijuana">Instagram</a></li>
-		</ul>
-	</div>
-	Proyecto en Colaboración con: 
-	<ul>
-		<li><a href="https://www.garitacenter.com/">Reporte de Garitas</a></li>
-		<li><a href="https://www.feedmetj.com/">Restaurantes en Tijuana</a></li>
-		<li><a href="https://www.noticiasmexico.org/">Las Últimas noticias de México</a></li>
-		<li><a href="https://www.comprarcasatijuana.com/">Comprar Casa en Tijuana</a></li>
-		<li><a href="https://www.larutadelvinoensenada.com/">La Ruta del Vino Ensenada</a></li>
-		<li><a href="https://www.mintitmedia.com/">Desarrollo Web en Tijuana</a></li>
-	</ul>
+  <div>
+    Síguenos en:
+    <ul>
+      <li>
+        <a
+          href="https://www.instagram.com/playamitijuana/"
+          title="Restaurantes en Playas de Tijuana">Instagram</a
+        >
+      </li>
+    </ul>
+  </div>
+  Proyecto en Colaboración con:
+  <ul>
+    <li><a href="https://www.garitacenter.com/">Reporte de Garitas</a></li>
+    <li><a href="https://www.feedmetj.com/">Restaurantes en Tijuana</a></li>
+    <li>
+      <a href="https://coupons.garitacenter.com/">
+        Papa Johns Coupons Promo Codes
+      </a>
+    </li>
+    <li>
+      <a href="https://www.larutadelvinoensenada.com/"
+        >La Ruta del Vino Ensenada</a
+      >
+    </li>
+    <li>
+      <a href="https://www.mintitmedia.com/">Desarrollo Web en Tijuana</a>
+    </li>
+  </ul>
 </footer>
+
+<style>
+  main,
+  footer {
+    position: relative;
+    margin: 0 auto;
+    box-sizing: border-box;
+    height: auto;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+</style>
